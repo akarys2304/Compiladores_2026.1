02f9db9f46fae70dd8a99be6c8ee03ce7df92881
Karys Barbosa <karys.barbosa@outlook.com>
2026-04-09 20:02:38 -0300

    inicio t2

---
+++ T2/src/main/java/br/ufscar/dc/compiladores/t2/App.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.compiladores.t2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
